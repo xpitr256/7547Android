f469dc883a6622d00458758608dc8f5b2e645576
sebaber <vicario.sebastian@gmail.com>
2017-03-11 16:50:54 -0300

    Agregado de boton "Obtener Usuario"

---
+++ Client/app/src/main/java/com/example/tallerdyp2/client/InitialActivity.java
@@ -0,0 +1,84 @@
+package com.example.tallerdyp2.client;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+
+        Button mSignInButton = (Button) findViewById(R.id.obtener_usuario);
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getUserInfo();
+            }
+        });
+
+//        Button mSignInButton = (Button) findViewById(R.id.iniciar_sesion);
+//        mSignInButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intentLogIn = new Intent(InitialActivity.this, LoginActivity.class );
+//                startActivity(intentLogIn);
+//            }
+//        });
+//
+//        Button mRegisterButton = (Button) findViewById(R.id.crear_cuenta);
+//        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intentRegister = new Intent(InitialActivity.this, RegisterActivity.class );
+//                startActivity(intentRegister);
+//            }
+//        });
+    }
+    @Override
+    public void onBackPressed() {}
+
+    public void getUserInfo() {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = getResources().getString(R.string.http_ip) + "/user";
+
+        final Context context = getApplicationContext();
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(context, response.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsObjRequest);
+    }
+}
